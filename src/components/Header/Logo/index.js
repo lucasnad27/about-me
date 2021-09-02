614e20df88d02ab7d0bc0b2402dfdfca068ed2a5
Lucas Culbertson <lucasnad27@gmail.com>
2021-09-02 01:32:03 -0600

    Dump from original starter-pack

---
+++ src/components/Header/Logo/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import { GatsbyImage } from "gatsby-plugin-image";
+
+import * as Styled from './styles';
+
+const Logo = () => {
+  const { site, placeholderImage } = useStaticQuery(graphql`{
+  site {
+    siteMetadata {
+      title
+    }
+  }
+  placeholderImage: file(relativePath: {eq: "profile.jpg"}) {
+    childImageSharp {
+      gatsbyImageData(width: 80, layout: CONSTRAINED)
+    }
+  }
+}
+`);
+
+  const logoTitle = site.siteMetadata.title;
+  const logoImage = placeholderImage.childImageSharp.gatsbyImageData;
+
+  return (
+    <Styled.Logo to="/">
+      <Styled.Image>
+        <GatsbyImage image={logoImage} alt={logoTitle} />
+      </Styled.Image>
+      <Styled.Text>{logoTitle}</Styled.Text>
+    </Styled.Logo>
+  );
+};
+
+export default Logo;
---
+++ src/components/Testimonials/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import { GatsbyImage } from "gatsby-plugin-image";
+import Loadable from '@loadable/component';
+
+import Container from 'components/ui/Container';
+import TitleSection from 'components/ui/TitleSection';
+import FormatHtml from 'components/utils/FormatHtml';
+
+import * as Styled from './styles';
+
+const Carousel = Loadable(() => import('components/ui/Carousel'));
+
+const Testimonials = () => {
+  const { markdownRemark, allMarkdownRemark } = useStaticQuery(graphql`{
+  markdownRemark(frontmatter: {category: {eq: "testimonials section"}}) {
+    frontmatter {
+      title
+      subtitle
+    }
+  }
+  allMarkdownRemark(filter: {frontmatter: {category: {eq: "testimonials"}}}) {
+    edges {
+      node {
+        id
+        html
+        frontmatter {
+          title
+          cover {
+            childImageSharp {
+              gatsbyImageData(width: 80, layout: CONSTRAINED)
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`);
+
+  const sectionTitle = markdownRemark.frontmatter;
+  const testimonials = allMarkdownRemark.edges;
+
+  return (
+    <Container section>
+      <TitleSection title={sectionTitle.title} subtitle={sectionTitle.subtitle} center />
+      <Styled.Testimonials>
+        <Carousel>
+          {testimonials.map((item) => {
+            const {
+              id,
+              html,
+              frontmatter: { cover, title }
+            } = item.node;
+
+            return (
+              <Styled.Testimonial key={id}>
+                <Styled.Image>
+                  <GatsbyImage image={cover.childImageSharp.gatsbyImageData} alt={title} />
+                </Styled.Image>
+                <Styled.Title>{title}</Styled.Title>
+                <FormatHtml content={html} />
+              </Styled.Testimonial>
+            );
+          })}
+        </Carousel>
+      </Styled.Testimonials>
+    </Container>
+  );
+};
+
+export default Testimonials;
